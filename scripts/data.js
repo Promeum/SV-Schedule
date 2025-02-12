4ee7a008408b55db14e1e32c7a0c3b562d87828a
Promeum <jakob.rogelio@gmail.com>
2025-02-12 15:46:39 -0500

    Schedule upd.

--- scripts/data.js
+++ scripts/data.js
@@ -349,6 +349,7 @@ const calendarDatabase = {
 
     '2/4/2025': ['EagleTime', 'Eagle Time', 'MP2 Report Cards in Synergy'],
     '2/6/2025': ['SpecialDelayedOpening', 'Delayed Opening', 'MP2 Report Cards Distributed'],
+    '2/12/2025': ['None', 'Snow Day'],
     '2/17/2025': ['None', 'President\'s Day', 'Schools and offices closed'],
     '2/28/2025': ['EarlyRelease', 'Early Release', 'MP3 Interims, Final grades in by 5 pm!'],
     
